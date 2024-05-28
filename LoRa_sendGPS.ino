7b39545bda50959dfddafdf3fd03fc0c2b407066
Imazu Naoto <108791046+ordonese@users.noreply.github.com>
2024-05-28 20:44:38 +0900

    Create LoRa_sendGPS.ino

---
+++ LoRa_sendGPS.ino
@@ -0,0 +1,43 @@
+#include <LoRa.h>
+#include "fromR_toL_gpsuart.h"
+
+String GPS_x;
+String GPS_y;
+String send_data;
+int send_data_int;
+float send_data_float;
+
+float ch = C24;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  Serial.println("LoRa sender");
+  if (!LoRa.begin(ch)) {  //C24~C38
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if(Serial1.available() > 0){
+    
+    GPS_x = Serial1.readStringUntil(',');
+    GPS_y = Serial1.readStringUntil('\n');
+    send_data = "latitude  = " + GPS_x + ", longitude = " + GPS_y;
+
+    // send packet
+    LoRa.beginPacket();  //パケットの送信を開始
+    LoRa.print(send_data);
+    LoRa.endPacket();  //パケットの送信を終了
+    Serial.print("LoRa.beginPacket  →  ");  
+    Serial.println(send_data);
+    Serial.print(" , by ");
+    Serial.print(ch/1000000);
+    Serial.print("MHz");
+    //Serial.println("LoRa.endPacket");
+  }
+}
