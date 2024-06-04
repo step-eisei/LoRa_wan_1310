02db13f7abbc5e4dbf5b0f901b36e9aac05063e1
Imazu Naoto <108791046+ordonese@users.noreply.github.com>
2024-06-04 22:17:33 +0900

    Create LoRa_sender.ino

---
+++ LoRa_sender.ino
@@ -0,0 +1,35 @@
+#include <LoRa.h>
+#include "fromR_toL_gpsuart.h"
+
+int send_data = 0;
+
+float ch = C24;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  Serial.println("LoRa sender");
+  if (!LoRa.begin(ch)) {  //C24~C38
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  send_data += 1;
+  if(send_data >= 10){
+    send_data = 0;
+  }
+  
+  // send packet
+  LoRa.beginPacket();  //パケットの送信を開始
+  LoRa.print(send_data);
+  LoRa.endPacket();  //パケットの送信を終了
+  Serial.print("LoRa.beginPacket  →  ");  
+  Serial.println(send_data);
+  Serial.print(" , by ");
+  Serial.print(ch/1000000);
+  Serial.print("MHz");
+  Serial.println("LoRa.endPacket");
+}
